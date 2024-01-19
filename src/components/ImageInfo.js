eadd80ba3aac1cb6ae52dd856c0712a1da8bcb78
yerinra <yerinaleph@gmail.com>
2024-01-19 23:09:35 +0900

    💄 Feat: fade in-out 효과 구현

--- src/components/ImageInfo.js
+++ src/components/ImageInfo.js
@@ -6,6 +6,8 @@ export default function ImageInfo({ $target, onExit }) {
   this.setState = (newData) => {
     this.data = newData;
     this.render();
+    // console.log('image info');
+    // console.log(this.data);
   };
 
   this.render = () => {
@@ -23,25 +25,43 @@ export default function ImageInfo({ $target, onExit }) {
             <div>tags : ${tags_preview.map((tag) => `<span>${tag.title}</span>`)}</div>
           </div>
         </div>`;
+      this.$modal.classList.add('fadein');
       this.$modal.style.display = 'block';
     } else {
       this.$modal.style.display = 'none';
     }
 
+    this.fadeInOut = () => {
+      // 뒤로가기가 클릭됐을 때, fadeout 추가, fadein이 있다면 삭제
+      this.$modal.classList.add('fadeout');
+      const effect = setInterval(() => {
+        if (this.$modal.classList.contains('fadein')) {
+          this.$modal.classList.remove('fadein');
+        }
+        // ImageInfo의 opacity(불투명도)가 0이 될 때까지 반복호출
+        if (this.$modal.style.opacity == 0) {
+          this.$modal.classList.remove('fadeout');
+          clearInterval(effect);
+          onExit();
+        }
+      }, 500);
+    };
+
     this.$modal.addEventListener('click', (e) => {
       const $btn = e.target.closest('.close');
-      const $imageInfo = e.target.closest('.ImageInfo');
+      // const $imageInfo = e.target.closest('.ImageInfo');
       const $wrapper = e.target.closest('.content-wrapper');
 
       if ($wrapper && !$btn) return;
-      // if ($imageInfo) {
-      onExit();
-      // }
+
+      // onExit();
+      this.fadeInOut();
     });
 
     document.addEventListener('keydown', (e) => {
       if (e.key === 'Escape') {
-        onExit();
+        this.fadeInOut();
+        // onExit();
       }
     });
   };
